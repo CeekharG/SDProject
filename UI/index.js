eed9eb7c0271e5d6691f3bbe588f815aa8c73d2c
CeekharG <shikhar.giri372@gmail.com>
2020-07-07 21:34:22 -0500

    Updated the commented line

--- UI/index.js
+++ UI/index.js
@@ -9,7 +9,4 @@ function hideURLbar() {
   window.scrollTo(0, 1);
 }
 
-var totGallon = document.getElementById("gallonreq");
-var price = document.getElementById("sugprice");
 
-document.getElementById("deuamount").value = 0 || totGallon * price;
