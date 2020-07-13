6efc1b411b15405427883798a5a3581323f49cbd
psimkhad <55363132+psimkhad@users.noreply.github.com>
2020-07-12 23:06:13 -0500

    Update views.py

--- Django_Backend/mainapp/views.py
+++ Django_Backend/mainapp/views.py
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
-from .forms import LoginForm, RegisterForm, UserProfileForm, FuelQUoteForm
-from django.http import HttpResponseRedirect
-from django.shortcuts import render
-from django.views import generic
-from django.contrib import messages
-=======
 from datetime import datetime
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib import messages
 from .models import *
 from .forms import LoginForm, RegisterForm, UserProfileForm, FuelQUoteForm
->>>>>>> BugFix
 
 
 class PriceModule:
@@ -22,21 +14,11 @@ class PriceModule:
 
 
 def login(request):
-<<<<<<< HEAD
-    initial_data = {'username': 'demouser', 'password': '12345'}
-=======
     initial_data = {'username': '', 'password': ''}
->>>>>>> BugFix
 
     if request.method == 'POST':
         form = LoginForm(request.POST)
 
-<<<<<<< HEAD
-        if form.is_valid() and form.cleaned_data['password'] == '12345':
-            return HttpResponseRedirect('/quote')
-        else:
-            messages.error(request, "Invalid Password!")
-=======
         if form.is_valid() and is_username_valid(form.cleaned_data['username']) and is_password_valid(form.cleaned_data['password']):
             userexist = UserCredentials.objects.filter(
                 username=form.cleaned_data['username'], password=form.cleaned_data['password']).exists()
@@ -61,26 +43,12 @@ def login(request):
                 messages.error(request, "Invalid Username or Password!")
         else:
             messages.error(request, "Invalid Username or Password!")
->>>>>>> BugFix
     else:
         form = LoginForm(initial=initial_data)
 
     return render(request, 'login.html', {'form': form.as_p})
 
 
-<<<<<<< HEAD
-def register(request):
-    initial_data = {'username': 'newuser',
-                    'password': '', 'confirm_password': ''}
-
-    if request.method == 'POST':
-        form = RegisterForm(request.POST)
-
-        if form.is_valid() and form.cleaned_data['password'] == form.cleaned_data['confirm_password']:
-            return HttpResponseRedirect('/create_profile')
-        else:
-            messages.error(request, "Password did not match!")
-=======
 def logout(request):
 
     if request.method == 'GET':
@@ -109,7 +77,6 @@ def register(request):
         else:
             messages.error(
                 request, "Short Password or Password did not match!")
->>>>>>> BugFix
     else:
         form = RegisterForm(initial=initial_data)
 
@@ -117,24 +84,6 @@ def register(request):
 
 
 def user_profile(request):
-<<<<<<< HEAD
-    initial_data = {'fullname': 'New User', 'address_1': 'Houston, Texas',
-                    'address_2': 'Houston, Texas', 'city': 'Houston', 'state': 'TX', 'zip': '123456'}
-
-    if request.method == 'POST':
-        form = UserProfileForm(request.POST)
-
-        if form.is_valid():
-            if (is_fullname_valid(form.cleaned_data['fullname']) and
-                is_address_valid(form.cleaned_data['address_1']) and
-                is_address2_valid(form.cleaned_data['address_2']) and
-                is_city_valid(form.cleaned_data['city']) and
-                is_state_valid(form.cleaned_data['state']) and
-                    is_zip_valid(form.cleaned_data['zip'])):
-                return HttpResponseRedirect('/quote')
-            else:
-                messages.error(request, "Data you entered is not valid!")
-=======
     initial_data = {'fullname': '', 'address_1': '',
                     'address_2': '', 'city': '', 'state': '', 'zip': ''}
 
@@ -182,7 +131,6 @@ def user_profile(request):
             return HttpResponseRedirect('/login')
         else:
             messages.error(request, "Data you entered is not valid!")
->>>>>>> BugFix
 
     else:
         form = UserProfileForm(initial=initial_data)
@@ -191,39 +139,6 @@ def user_profile(request):
 
 
 def fuel_quote(request):
-<<<<<<< HEAD
-    data = {'gallonreq': '2000', 'deladdress': 'Houston, Texas',
-            'deliverydate': '2020-06-27', 'suggprice': '145.59', 'deuamount': '290000'}
-    if request.method == 'POST':
-        form = FuelQUoteForm(request.POST)
-
-        if form.is_valid() and int(form.cleaned_data['gallonreq']) > 0:
-            return HttpResponseRedirect('/history')
-        else:
-            messages.error(request, "Data you entered is not valid!")
-
-    else:
-        form = FuelQUoteForm(initial=data)
-
-    return render(request, 'fuel-quote.html', {'form': form, 'loginuser': 'demouser', 'quote_active': True})
-
-
-def fuel_quote_history(request):
-    data = [{"id": 1, "req_gallons": 152,
-             "del_address": "Houston, Texas", "delivery_date": "July 1, 2020", "sugg_price": 2, "total_amount": 0},
-            {"id": 2, "req_gallons": 140,
-             "del_address": "San Francisco, California", "delivery_date": "July 2, 2020", "sugg_price": 3, "total_amount": 0},
-            {"id": 3, "req_gallons": 600,
-             "del_address": "Brooklyn, New York", "delivery_date": "July 3, 2020", "sugg_price": 1, "total_amount": 0},
-            {"id": 4, "req_gallons": 582,
-             "del_address": "Manhattan, New York", "delivery_date": "July 4, 2020", "sugg_price": 3, "total_amount": 0},
-            {"id": 5, "req_gallons": 156,
-             "del_address": "Austin, Texas", "delivery_date": "July 5, 2020", "sugg_price": 2, "total_amount": 0},
-            {"id": 6, "req_gallons": 175,
-             "del_address": "Dallas, Texas", "delivery_date": "July 6, 2020", "sugg_price": 2, "total_amount": 0}]
-    if request.method == 'GET':
-        return render(request, 'fuel-quote-history.html', {'data': data, 'loginuser': 'demouser', 'history_active': True})
-=======
     session = False
     user = int(request.session['id'])
     session_exist = Sessions.objects.filter(
@@ -294,7 +209,6 @@ def is_username_valid(username):
 
 def is_password_valid(password):
     return len(password) > 5 and len(password) <= 50
->>>>>>> BugFix
 
 
 def is_fullname_valid(fullname):
