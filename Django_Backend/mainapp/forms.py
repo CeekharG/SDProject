de071bf00165e54bc3348c65aab215d7f4f79e7d
psimkhad <55363132+psimkhad@users.noreply.github.com>
2020-07-12 23:05:57 -0500

    Update forms.py

--- Django_Backend/mainapp/forms.py
+++ Django_Backend/mainapp/forms.py
@@ -1,8 +1,5 @@
 from django import forms
-<<<<<<< HEAD
-=======
 from .models import States
->>>>>>> BugFix
 
 
 class LoginForm(forms.Form):
@@ -25,23 +22,12 @@ class RegisterForm(forms.Form):
 
 
 class UserProfileForm(forms.Form):
-<<<<<<< HEAD
-    OPTIONS = (
-        ('', 'Select State'),
-        ('TX', 'Texas'),
-        ('NY', 'New York'),
-        ('SD', 'South Dakota'),
-        ('ND', 'North Dakota'),
-        ('CA', 'California'),
-    )
-=======
     OPTIONS = [('', 'Select State')]
     if(States):
         states = States.objects.all()
         for state in states:
             OPTIONS.append((state.code, state.name))
 
->>>>>>> BugFix
     fullname = forms.CharField(label='Full Name', required=False, max_length=50,
                                widget=forms.TextInput(attrs={'placeholder': "Full Name"}))
 
@@ -66,27 +52,14 @@ class FuelQUoteForm(forms.Form):
     gallonreq = forms.CharField(required=True,
                                 widget=forms.NumberInput(attrs={'class': 'form-control', 'step': '1'}))
 
-<<<<<<< HEAD
-    deladdress = forms.CharField(required=False, disabled=True,
-                                 widget=forms.TextInput(attrs={'class': 'form-control'}))
-=======
     deladdress = forms.CharField(required=False,
                                  widget=forms.TextInput(attrs={'readonly': '', 'class': 'form-control'}))
->>>>>>> BugFix
 
     deliverydate = forms.CharField(required=False,
                                    widget=forms.DateTimeInput(attrs={'class': 'form-control', 'type': 'date'}))
 
-<<<<<<< HEAD
-    suggprice = forms.CharField(required=False, disabled=True,
-                                widget=forms.NumberInput(attrs={'class': 'form-control', 'step': 'any'}))
-
-    deuamount = forms.CharField(required=False, disabled=True,
-                                widget=forms.NumberInput(attrs={'class': 'form-control', 'step': 'any'}))
-=======
     suggprice = forms.CharField(required=False,
                                 widget=forms.NumberInput(attrs={'readonly': '', 'class': 'form-control', 'step': 'any'}))
 
     deuamount = forms.CharField(required=False,
                                 widget=forms.NumberInput(attrs={'readonly': '', 'class': 'form-control', 'step': 'any'}))
->>>>>>> BugFix
